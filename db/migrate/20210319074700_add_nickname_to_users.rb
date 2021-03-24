86d93dd7985fad84f47354f5cc81ed9d1537ea39
rei-01-18 <80232197+rei-01-18@users.noreply.github.com>
2021-03-24 18:37:56 +0900

    マイグレーションファイルの修正

--- db/migrate/20210319074700_add_nickname_to_users.rb
+++ db/migrate/20210319074700_add_nickname_to_users.rb
@@ -2,7 +2,7 @@ class AddNicknameToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :nickname, :string
     add_column :users, :profile, :text
-    add_column :users, :posision, :text
+    add_column :users, :position, :text
     add_column :users, :occupation, :text
   end
 end
